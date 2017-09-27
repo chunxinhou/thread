cff55edb2287ba69c3065c5c0aeb8f7f40dabd0d
候春鑫 <chunxinhou@foxmail.com>
2017-09-27 11:16:35 +0800

    创建idea工程

---
+++ src/com/thread/test/Test.java
@@ -0,0 +1,12 @@
+package com.thread.test;
+
+/**
+ * Created by hcx on 2017/9/27.
+ */
+public class Test {
+
+    public  static void main(String[] args){
+        System.out.println("hello thread");
+    }
+
+}
